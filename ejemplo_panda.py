cccf65e37abd8bf916843ee1bb0333b862f2eea2
Josué <josue.alex.urrutia@gmail.com>
2026-05-09 15:00:06 -0500

    Delete ejemplo_panda.py

--- ejemplo_panda.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Creamos una lista de ventas rápida
-ventas = {'Producto': ['Manzanas', 'Peras'], 'Cantidad': [10, 5]}
-df = pd.DataFrame(ventas)
-
-print("¡Hola! Tu DataFrame de Pandas funciona:")
-print(df)
